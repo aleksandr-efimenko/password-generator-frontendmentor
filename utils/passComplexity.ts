0172773c4cefbd65b1820f0771e900d4e499eab8
androidblog <androidblog@yandex.ru>
2023-04-24 12:55:48 +0400

    create passwords complexity checker

--- utils/passComplexity.ts
+++ utils/passComplexity.ts
@@ -1,11 +1,45 @@
 export enum PassStrength {
   TooWeak,
   Weak,
-  Medium, 
+  Medium,
   Strong,
 }
 
 export function determinePasswordComplexity(password: string): PassStrength {
-  let complexity = PassStrength.Weak;
-  return complexity;
+  const lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+  const uppercaseLetters = lowercaseLetters.toUpperCase();
+  const numbersString = "0123456789";
+  const symbolsString = "!@#$%^&*()_+~`|}{[]:;?><,./-=";
+
+  const lowercaseRegex = /[a-z]/;
+  const uppercaseRegex = /[A-Z]/;
+  const numbersRegex = /\d/;
+  const symbolsRegex = /[!@#$%^&*()_+~`|}{[\]:;?/><,.\/-=]/;
+
+  let entropy = 0;
+  if (lowercaseRegex.test(password)) {
+    entropy += lowercaseLetters.length;
+  }
+  if (uppercaseRegex.test(password)) {
+    entropy += uppercaseLetters.length;
+  }
+  if (numbersRegex.test(password)) {
+    entropy += numbersString.length;
+  }
+  if (symbolsRegex.test(password)) {
+    entropy += symbolsString.length;
+  }
+
+  const passwordEntropy = Math.log2(entropy) * password.length;
+
+  if (passwordEntropy < 28) {
+    return PassStrength.TooWeak;
+  }
+  if (passwordEntropy < 36) {
+    return PassStrength.Weak;
+  }
+  if (passwordEntropy < 60) {
+    return PassStrength.Medium;
+  }
+  return PassStrength.Strong;
 }
